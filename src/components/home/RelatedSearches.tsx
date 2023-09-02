434a7d15c0f50722ecfede8f28af355014a537b5
kabir5146 <fl.kabirhossain@gmail.com>
2023-09-02 15:19:46 +0600

    search slider done without responsive

--- src/components/home/RelatedSearches.tsx
+++ src/components/home/RelatedSearches.tsx
@@ -1,7 +1,26 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { createRef } from "react";
+import Slider from "react-slick";
 
 const RelatedSearches = () => {
+  const sliderRef = createRef<Slider>();
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    autoplaySpeed: 3000,
+    slidesToShow: 8.2,
+    slidesToScroll: 1,
+  };
+  const previous = () => {
+    sliderRef.current?.slickPrev();
+  };
+  const next = () => {
+    sliderRef.current?.slickNext();
+  };
+
   const searches = [
     {
       title: "floral wrap dress long",
@@ -35,27 +54,42 @@ const RelatedSearches = () => {
       title: "dolman wrap dress",
       url: "",
     },
+    {
+      title: "dolman wrap dress",
+      url: "",
+    },
   ];
   return (
     <div className="container mb-12">
       <p className="text-xl leading-8 font-bold text-[#000000] capitalize text-center mb-[23px]">
         Related Searches
       </p>
-      <div className="flex flex-row">
-        <button>
-          <i className="fa-solid fa-chevron-left"></i>
-        </button>
-        <div className="flex flex-row gap-x-5">
-          {searches.map((item: any, i: number) => (
-            <div key={i} className="">
-              <Link href={item.url} className="p-4 rounded-full bg-[#F2F2F3]">
-                {item.title}
-              </Link>
-            </div>
-          ))}
+      <div className="w-full flex flex-row relative">
+        <div className="flex flex-row">
+          <Slider className="w-[93vw]" ref={sliderRef} {...settings}>
+            {searches.map((item: any, i: number) => (
+              <div key={i} className="py-4">
+                <Link href={item.url} className="p-4 rounded-full bg-[#F2F2F3]">
+                  {item.title}
+                </Link>
+              </div>
+            ))}
+          </Slider>
         </div>
-        <button>
-          <i className="fa-solid fa-chevron-right"></i>
+
+        <button
+          type="button"
+          onClick={previous}
+          className="absolute left-3 top-[50%] -translate-y-1/2 rounded-full"
+        >
+          <i className="fa-solid fa-chevron-left z-10"></i>
+        </button>
+        <button
+          type="button"
+          onClick={next}
+          className="absolute right-3 top-[50%] -translate-y-1/2 rounded-full shadow-white"
+        >
+          <i className="fa-solid right-3 fa-chevron-right z-10"></i>
         </button>
       </div>
     </div>
