2328eca7062dde446e1af165d6189bd4763787f2
sanjid0612 <mdsahariarhossain06@gmail.com>
2023-09-04 14:33:05 +0600

    product details page create pause

---
+++ src/app/template.tsx
@@ -0,0 +1,25 @@
+import BottomHeader from '@/components/header/BottomHeader';
+import MiddleHeader from '@/components/header/MiddleHeader';
+import MobileHeader from '@/components/header/MobileHeader';
+import TopHeader from '@/components/header/TopHeader';
+import Footer from '@/components/home/Footer';
+import React from 'react'
+
+type TemplateProps = {
+    children: React.ReactNode;
+  };
+  const Template: React.FC<TemplateProps> = ({ children }) => {
+    console.log("Template");
+    return (
+      <div className="w-full">
+        <TopHeader />
+        <MobileHeader />
+        <MiddleHeader />
+        <BottomHeader />
+        <main className="">{children}</main>
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default Template;
---
+++ src/components/common/StarList.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+export type starProps={
+    review?:string;
+    icon:string[]
+}
+const StarList:FC<starProps> = ({review,icon}) => {
+  return (
+    <button type='button' className='text-sm flex text-yellow-500 flex-row justify-start items-center gap-x-1'>
+        {icon.map((item:string,id:number)=>(<i key={id} className={`${item}`}></i>))} 
+        <p className="pl-1 text-[#5D626A] hover:underline">({review})</p>         
+    </button>
+  )
+}
+
+export default StarList
