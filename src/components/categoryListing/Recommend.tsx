14c6a8b634f24ae24960a708810a7b6cc4a80550
kabir5146 <fl.kabirhossain@gmail.com>
2023-09-06 10:24:36 +0600

    filter added

--- src/components/categoryListing/Recommend.tsx
+++ src/components/categoryListing/Recommend.tsx
@@ -10,7 +10,7 @@ export type recommendProps = {
 
 const Recommend: FC<recommendProps> = ({ recommendArr }) => {
   return (
-    <div className="container my-10 flex flex-row items-center gap-x-3 lg:bg-[#F2F2F3] px-6 py-7">
+    <div className="hidden container my-10 3xl:flex flex-row items-center gap-x-3 lg:bg-[#F2F2F3] px-6 py-7">
       <p className="text-base capitalize text-[#222]">Recommend</p>
       <div className="flex flex-row flex-wrap items-center gap-3">
         {recommendArr.map((item: recommendType, i: number) => (
--- src/components/categoryListing/SideBar.tsx
+++ src/components/categoryListing/SideBar.tsx
@@ -1,15 +1,55 @@
 "use client"
-import React, { useState } from "react";
-import Checkbox from "../base/Checkbox";
+import React, { FC, useState } from "react";
 import FilterItem from "../common/FilterItem";
 
-const SideBar = () => {
-  
+type FilterItemType = {
+  title: string;
+  labels: Label[];
+};
+
+type Label = {
+  label: string;
+};
+
+const SideBar: FC = () => {
+  const [check, setCheck] = useState(true);
+
+  // Define an array of filter items with different titles and labels
+  const filterItems: FilterItemType[] = [
+    {
+      title: "Category 1",
+      labels: [
+        { label: "Label 1" },
+        { label: "Label 2" },
+      ],
+    },
+    {
+      title: "Category 2",
+      labels: [
+        { label: "Label 3" },
+        { label: "Label 4" },
+      ],
+    },
+    // Add more filter items as needed
+  ];
+
   return (
-  <div className="w-[216px] mr-6 text-black pr-3 hidden 3xl:block">
-    <h1 className="text-lg leading-10 pb-3 font-semibold border-b border-[#E0E1E3]">FILTERS</h1>
-    <FilterItem/>
-  </div>
+    <div className="w-[216px] mr-6 text-black pr-3 hidden 3xl:block">
+      <h1 className="text-lg leading-10 pb-3 font-semibold border-b border-[#E0E1E3]">FILTERS</h1>
+
+      {/* Use map to render FilterItem components for each filter item */}
+      {filterItems.map((item, index) => (
+        <FilterItem
+          key={index}
+          title={item.title}
+          actionCb={() => setCheck(!check)}
+          viewStateCb={check}
+          icon={`${check === false ? "rotate-180" : ""}`}
+          labels={item.labels}
+          actionCbView={() => {}}
+        />
+      ))}
+    </div>
   );
 };
 
