8bc0844d420724f344c0233e98dad27dcc02d2ae
sanjid0612 <mdsahariarhossain06@gmail.com>
2023-09-16 22:50:52 +0600

    mobile header done

--- src/components/header/MobileHeader.tsx
+++ src/components/header/MobileHeader.tsx
@@ -1,15 +1,15 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useState } from 'react';
-import SlideInOut from '../common/SlideInOut';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import SlideInOut from "../common/SlideInOut";
 
 const MobileHeader = () => {
   const [show, setShow] = useState(false);
   return (
     <>
-      <div className='lg:hidden flex flex-row justify-between items-center py-1.5 md:py-4 px-2 md:px-5 relative'>
-        <div className='flex flex-row items-center gap-3'>
+      <div className="lg:hidden flex flex-row justify-between items-center py-1.5 md:py-4 px-2 md:px-5 relative">
+        <div className="flex flex-row items-center gap-3">
           {/* <button
             type="button"
             onClick={() => setShow(!show)}
@@ -17,29 +17,45 @@ const MobileHeader = () => {
           >
             <i className="fas fa-bars"></i>
           </button> */}
-          <Link href={'/'} className='w-32 h-8 md:h-9 relative'>
+          <Link href={"/"} className="w-32 h-8 md:h-9 relative">
             <Image
-              src='https://backend-live.clearance.ae/storage/company/2023-02-06-63e08deba2852.png'
+              src="https://backend-live.clearance.ae/storage/company/2023-02-06-63e08deba2852.png"
               fill
-              alt='logo'
+              alt="logo"
             />
           </Link>
         </div>
-        <div className='flex flex-row items-center gap-x-2 text-xl md:text-2xl'>
-          <button type='button' className='group relative p-2 md:p-3'>
-            <i className='fa-regular fa-user'></i>
-          </button>
-          <button className='p-2'>
-            <i className='fa-solid fa-magnifying-glass text-black'></i>
-          </button>
-          <button type='button' className='group relative p-2 md:p-3'>
-            <i className='fa-solid fa-bag-shopping'></i>
-          </button>
+        <div className="flex flex-row items-center gap-x-2 text-xl md:text-2xl">
+          <Link
+            target={process.env.NEXT_PUBLIC_SITE_URL ? "_blank" : ""}
+            href={
+              process.env.NEXT_PUBLIC_SITE_URL
+                ? process.env.NEXT_PUBLIC_SITE_URL + "customer/auth/login"
+                : `customer/auth/login`
+            }
+            className="group relative p-2 md:p-3"
+          >
+            <i className="fa-regular fa-user"></i>
+          </Link>
+          {/* <button className="p-2">
+            <i className="fa-solid fa-magnifying-glass text-black"></i>
+          </button> */}
+          <Link
+            target={process.env.NEXT_PUBLIC_SITE_URL ? "_blank" : ""}
+            href={
+              process.env.NEXT_PUBLIC_SITE_URL
+                ? process.env.NEXT_PUBLIC_SITE_URL + "shop-cart"
+                : `shop-cart`
+            }
+            className="group relative p-2 md:p-3"
+          >
+            <i className="fa-solid fa-bag-shopping"></i>
+          </Link>
         </div>
       </div>
       <div
         className={`w-[80%] absolute transition-all duration-1000 ${
-          show ? 'left-0' : '-left-[80%]'
+          show ? "left-0" : "-left-[80%]"
         } `}
       >
         <SlideInOut />
@@ -49,4 +65,3 @@ const MobileHeader = () => {
 };
 
 export default MobileHeader;
-
