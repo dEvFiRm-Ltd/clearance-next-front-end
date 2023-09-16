8fe2eb0847216adc47680457ca732684dd5291c0
kabir5146 <fl.kabirhossain@gmail.com>
2023-09-16 21:14:12 +0600

    middle header updated

--- src/components/header/MiddleHeader.tsx
+++ src/components/header/MiddleHeader.tsx
@@ -136,13 +136,15 @@ const MiddleHeader = () => {
             )}
             {/* hover dropdown ends */}
           </button>
-          <button
-            type="button"
-            onClick={() => setIsCartOpen(true)}
-            className="relative p-2 2xl:p-3"
-          >
-            <i className="fa-solid fa-bag-shopping"></i>
-          </button>
+          {!process.env.NEXT_PUBLIC_SITE_URL && (
+            <button
+              type="button"
+              onClick={() => setIsCartOpen(true)}
+              className="relative p-2 2xl:p-3"
+            >
+              <i className="fa-solid fa-bag-shopping"></i>
+            </button>
+          )}
         </div>
       </div>
       <CartSideBar value={isCartOpen} setCart={setIsCartOpen} />
