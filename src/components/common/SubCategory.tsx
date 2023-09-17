a27d60ced573f4a1a0df6b8564f4092c64503083
sanjid0612 <mdsahariarhossain06@gmail.com>
2023-09-17 19:45:02 +0600

    SubCategory create

---
+++ src/components/common/SubCategory.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import React, { FC, useState } from "react";
+
+const SubCategory: FC<any> = ({
+  itemArr,
+  heading,
+  headingClass,
+  socialArr,
+  contactUsArr,
+  groupClass = "",
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className={groupClass}>
+      <div className="lg:hidden w-full border-b ">
+        <button
+          type="button"
+          onClick={() => setIsExpanded(!isExpanded)}
+          className={`w-full flex flex-row justify-between items-center uppercase text-base text-black py-4 ${headingClass}`}
+        >
+          <span>{heading}</span>
+          <i className="fas fa-plus text-sm"></i>
+        </button>
+        <div className="pl-4 flex flex-col justify-start gap-y-1">
+          {isExpanded &&
+            itemArr?.map((item: any, id: number) => (
+              <Link
+                target="_blank"
+                key={id}
+                href={item.url}
+                className="py-2 text-sm text-gray"
+              >
+                {item.title}
+              </Link>
+            ))}
+        </div>
+      </div>
+      {/* mobile & tab only */}
+      <h3
+        className={`hidden lg:block lg:text-base 2xl:text-lg font-bold text-black uppercase mb-5 ${headingClass}`}
+      >
+        {heading}
+      </h3>
+      <div className="hidden lg:flex flex-col justify-start gap-y-3">
+        {itemArr?.map((item: any, id: number) => (
+          <Link
+            key={id}
+            target="_blank"
+            href={item.url}
+            className="text-[13px] 2xl:text-sm text-black font-normal leading-5"
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SubCategory;
