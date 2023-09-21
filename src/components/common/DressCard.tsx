3a875d38c7a3e5842c60065ea8e52cbe8193a9cc
malinjr07 <malinjr07@gmail.com>
2023-09-21 19:09:48 +0600

    url fixed

--- src/components/common/DressCard.tsx
+++ src/components/common/DressCard.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import React, { FC } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react';
 
 export type dressType = {
   image: string;
@@ -16,8 +16,8 @@ const DressCard: FC<dressType> = ({
   image,
   url,
   title,
-  hightClass = "",
-  withClass = "",
+  hightClass = '',
+  withClass = '',
   heading,
   totalProduct,
 }) => {
@@ -25,27 +25,20 @@ const DressCard: FC<dressType> = ({
     <Link
       // target='_blank'
       // href={'process.env.NEXT_PUBLIC_SITE_URL/products?category=' + url}
-      target={process.env.NEXT_PUBLIC_SITE_URL ? "_blank" : ""}
-      href={
-        process.env.NEXT_PUBLIC_SITE_URL
-          ? process.env.NEXT_PUBLIC_SITE_URL +
-            "products?category=" +
-            url +
-            `&page=1`
-          : `/products?category=` + url
-      }
+      target={process.env.NEXT_PUBLIC_SITE_URL ? '_blank' : ''}
+      href={url || '/'}
       className={`xl:ml-0 xl:mr-0 xl:w-48 2xl:w-[234px] 3xl:w-[272px] flex flex-col items-center justify-start gap-y-4 uppercase ${withClass}`}
     >
       <div className={`relative md:aspect-[40/53] w-full ${hightClass}`}>
-        <Image src={image} alt="image" fill className="object-contain" />
+        <Image src={image} alt='image' fill className='object-contain' />
       </div>
       {heading && (
-        <p className="text-base md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl 2xl:leading-10 font-semibold text-black">
+        <p className='text-base md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl 2xl:leading-10 font-semibold text-black'>
           {heading}
         </p>
       )}
       {title && (
-        <p className="border-b text-gray border-[#B9B9B9] text-sm lg:text-base xl:text-lg 3xl:text-xl xl:leading-8">
+        <p className='border-b text-gray border-[#B9B9B9] text-sm lg:text-base xl:text-lg 3xl:text-xl xl:leading-8'>
           {title} ({totalProduct})
         </p>
       )}
@@ -54,3 +47,4 @@ const DressCard: FC<dressType> = ({
 };
 
 export default DressCard;
+
