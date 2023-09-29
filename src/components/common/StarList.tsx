62e247c413fcee95af132457878714c079da3bdf
sanjid0612 <mdsahariarhossain06@gmail.com>
2023-09-29 20:59:19 +0600

    missing design add

--- src/components/common/StarList.tsx
+++ src/components/common/StarList.tsx
@@ -1,22 +1,28 @@
-import React, { FC } from 'react'
-export type starProps={
-    review?:string;
-    
-}
-const StarList:FC<starProps> = ({review}) => {
-  const star= [
+import React, { FC } from "react";
+export type starProps = {
+  review?: string;
+  groupClass?: string;
+  starClass?: string;
+};
+const StarList: FC<starProps> = ({ review, groupClass, starClass }) => {
+  const star = [
     "fas fa-star",
     "fas fa-star",
     "fas fa-star",
     "fas fa-star",
     "fas fa-star",
-  ]
+  ];
   return (
-    <button type='button' className='text-sm flex text-yellow-500 flex-row justify-start items-center gap-x-1'>
-        {star.map((item:string,id:number)=>(<i key={id} className={`${item}`}></i>))} 
-        <p className="pl-1 text-gray hover:underline">({review})</p>         
+    <button
+      type="button"
+      className={`text-sm flex text-yellow-500 flex-row justify-start items-center gap-x-1 ${groupClass}`}
+    >
+      {star.map((item: string, id: number) => (
+        <i key={id} className={`${starClass} ${item}`}></i>
+      ))}
+      <p className="pl-1 text-gray hover:underline">({review})</p>
     </button>
-  )
-}
+  );
+};
 
-export default StarList
+export default StarList;
