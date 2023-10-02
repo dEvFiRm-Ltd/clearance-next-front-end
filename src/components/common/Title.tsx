7ae0a2326ef2de93737354c8afc462738b725d6f
sanjid0612 <mdsahariarhossain06@gmail.com>
2023-10-02 13:51:42 +0600

    arrow button mt add

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -7,7 +7,7 @@ const Title = () => {
   const path = usePathname();
   const local = path.split("/")[1];
   return (
-    <div className="w-full flex flex-col lg:flex-row lg:justify-between lg:items-center justify-start gap-y-5 items-center px-10 mx-auto container pt-5 lg:pb-4">
+    <div className="w-full flex flex-col lg:flex-row lg:justify-between justify-start gap-y-5 items-center px-10 mx-auto container pt-5 lg:pb-4">
       <h3 className="font-bold text-base lg:text-xl 2xl:text-2xl 3xl:text-[28px] capitalize ">
         {local === "en" ? "flash sale" : "بيع مفاجئ"}
       </h3>
@@ -18,7 +18,7 @@ const Title = () => {
         <TimeCountdown date={"2023-10-08T15:27:32.635Z"} />
         <Link
           href={""}
-          className="hidden bg-[#ffffff88] lg:h-9 lg:w-9 2xl:h-10 2xl:w-10 rounded-full lg:flex justify-center items-center text-gray-800 "
+          className="hidden bg-[#ffffff88] lg:h-9 lg:w-9 2xl:h-10 2xl:w-10 rounded-full mt-[1%] 2xl:mt-[1.5%] lg:flex justify-center items-center text-gray-800 "
         >
           <i className="fas fa-chevron-right"></i>
         </Link>
